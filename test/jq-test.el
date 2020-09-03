c1bb5cc66faf4197af275108d36b5ac007882db4
p-baleine <p.baleine@gmail.com>
2020-09-04 02:46:31 +0900

    Add error handlings.
    closes #1.

--- test/jq-test.el
+++ test/jq-test.el
@@ -21,3 +21,25 @@
     (should (= (length actual) 2))
     (should (string= (cdr (assoc 'town (nth 0 actual))) "Onett"))
     (should (string= (cdr (assoc 'town (nth 1 actual))) "Twoson"))))
+
+(ert-deftest jq-test--jq--signaling-on-invalid-json-string ()
+  (let* ((input "
+[
+  {
+    \"name\": \"Ness\",
+    \"age\": 12
+    \"origin\": { \"country\": \"Eagleland\", \"town\": \"Onett\" }
+  }
+]
+")
+         (program ".[] | {town: .origin.town}")
+         err)
+    (condition-case e
+        (iter-next (jq input program))
+      (error (setq err e)))
+
+    (should (eq (car err) 'error))
+    (should
+     (string=
+      (cdr err)
+      "jq: error: Expected separator between values at line 6, column 12"))))
